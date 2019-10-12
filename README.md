258a97e2a575c6d71ce151885adc3965d0ae0def
jiangyupeng <jiangyupeng@bjsasc.com>
2019-10-12 11:12:28 +0800

    RUNNABLE

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # basic
-java
+《java编程思想》
+java 多线程
+第21章 并发
+21.2.1 定义任务
+LiftOff.java
--- src/main/java/com/chiang/task/LiftOff.java
+++ src/main/java/com/chiang/task/LiftOff.java
@@ -2,15 +2,43 @@ package com.chiang.task;
 
 public class LiftOff implements Runnable {
 
+	protected int countDown = 10;
+	private static int taskCount = 0;
+	private final int id = taskCount++;
+
+	/**
+	 * 
+	 */
+	public LiftOff() {
+	}
+
+	/**
+	 * @param countDown
+	 */
+	public LiftOff(int countDown) {
+		this.countDown = countDown;
+	}
+
+	public String status() {
+		return "#" + id + "(" + (countDown > 0 ? countDown : "LiftOff!") + "),";
+	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		while (countDown-- > 0) {
+			System.out.print(status());
+			Thread.yield();
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * LiftOff launch = new LiftOff(); launch.run();
+		 */
+		for (int i = 0; i < 5; i++) {
+			new Thread(new LiftOff()).start();
+		}
+		System.out.println("Waiting for LiftOff");
 	}
 
 }
